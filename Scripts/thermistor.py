9e61a8884c08f67996bff4842cf9839eff0c363a
lolmile <lolmile32@gmail.com>
2023-09-22 10:05:06 -0400

    create function for temp

--- Scripts/thermistor.py
+++ Scripts/thermistor.py
@@ -31,3 +31,17 @@ if __name__ == '__main__':
         ADC0832.destroy()
         logging.info("Stopping...")
         print ('The end !')
+
+
+def getTemp():
+  res = ADC0832.getADC(0)
+  Vr = 3.3 * res / 255
+  Vr2 = 3.3 * float(res) / 255
+  Rt = (10000 * 3.3 /  Vr) - 10000
+    
+  temp = 1/(((math.log(Rt / 10000)) / 3455) + (1 / (273.15+25)))
+  Cel = temp - 273.15
+  Fah = Cel * 1.8 + 32
+  time.sleep(0.2)
+
+  return Cel
